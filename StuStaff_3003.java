b7ac0a2da7ed8957cf36a85ec03825c241a0cf79
ariyosheva <115518111+ariyosheva@users.noreply.github.com>
2022-12-11 13:10:41 +0700

    Update StuStaff_3003.java

--- StuStaff_3003.java
+++ StuStaff_3003.java
@@ -2,28 +2,44 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompanyject.main_3003;
+package com.mycompanyject.uts_no3_05a_3003;
 
 /**
  *
  * @author lenovo
  */
-public class StuStaff_3003 extends Mahasiswa_3003 {
-    
-    public int unitKerja_3003;
-    public int jamKerja_3003;
-    
-    public StuStaff_3003 (String nama_3003,String nim_3003,String jurusan_3003,int ipk_3003,int unitKerja_3003,int jamKerja_3003){
-        super(nama_3003,nim_3003,jurusan_3003,ipk_3003);
-        this.unitKerja_3003 = unitKerja_3003;
+public class StudentStaff_3003 extends Mahasiswa_3003 implements iPendapatan_3003 {
+
+    String unitKerja_3003;
+    int jamKerja_3003;
+    double totalPendapatan_3012;
+
+    public StudentStaff_3003() {
+
+    }
+
+    public StudentStaff_3003(String nim, String nama, String Jurusan, int ipk,
+            String unitKerja, int jamKerja) {
+        super(nim, nama, Jurusan, ipk);
         this.jamKerja_3003 = jamKerja_3003;
     }
-    public double totalPendapatan(){
-        return jamKerja_3003 * 30000;
-    }
+    
     public void tampilDataMhs(){
-        System.out.println("Unit kerja : " + unitKerja_3003);
-        System.out.println("Jam kerja : " + jamKerja_3003);
-        
+        super.tampilDataMhs();
+        infostudent();
     }
+    
+    public void infostudent(){
+        System.out.println("unitKerja : Fakultas Informatika" + unitKerja_3003);
+        System.out.println("jamKerja : 12" + jamKerja_3003);
+    }
+
+    @Override
+    public double totalPendapatan_3003() {
+        int totalPendapatan_3003;
+        return totalPendapatan_3003 = (int) (jamKerja_3003 *30000);
+ 
+}
+
+    
 }
