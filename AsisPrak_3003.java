96acf8beb87dbf650c10ac2ecad9ad0709e8caa0
ariyosheva <115518111+ariyosheva@users.noreply.github.com>
2022-12-11 13:10:23 +0700

    Update AsisPrak_3003.java

--- AsisPrak_3003.java
+++ AsisPrak_3003.java
@@ -2,27 +2,44 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompanyject.main_3003;
+package com.mycompanyject.uts_no3_05a_3003;
 
 /**
  *
  * @author lenovo
  */
-public class AsisPrak_3003 extends Mahasiswa_3003 {
-  
-    public String mkAsistensi_3003;
-    public int jmlPertemuan_3003;
+public class Asprak_3003  extends Mahasiswa_3003 implements iPendapatan_3003{
+    String MKasistensi_3003;
+    int jmlhPertemuan_3003;
+    double totalPendapatan_3003;
+
+    public Asprak_3003(){
+        
+    }
     
-    public AsisPrak_3003(String nama_3003,String nim_3003,String jurusan_3003,int ipk_3003,String mkAsistensi_3003,int jmlPertemuan_3003){
-        super(nama_3003,nim_3003,jurusan_3003,ipk_3003);
-        this.mkAsistensi_3003 = mkAsistensi_3003;
-        this.jmlPertemuan_3003 = jmlPertemuan_3003;
+    public Asprak_3003(String nim, String nama, String Jurusan, int ipk, 
+            String MKasistensi, int jmlhPertemuan){
+        super(nim,nama,Jurusan,ipk);
+        this.MKasistensi_3003 = MKasistensi_3003;
+        this.jmlhPertemuan_3003 = jmlhPertemuan_3003;
+        
     }
-    public double totalPendapatan(){
-        return jmlPertemuan_3003 * 50000;
+    
+    public void infoAsprak(){
+        System.out.println("MKasistensi : PBO" + MKasistensi_3003);
+        System.out.println("jmlhPertemuan : 2" + jmlhPertemuan_3003);
     }
+    
     public void tampilDataMhs(){
-        System.out.println("Mata kuliah : " +mkAsistensi_3003);
-        System.out.println("Jumlah pertemuan : " + jmlPertemuan_3003);
+        super.tampilDataMhs();
+        infoAsprak();
     }
+    
+    public double totalPendapatan_3003(){
+        return totalPendapatan_3003 = (jmlhPertemuan_3003 *50000);
+        
+ 
+}
+    
+
 }
