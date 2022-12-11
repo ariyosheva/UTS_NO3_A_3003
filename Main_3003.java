f2738a95c6f18dedd0fd2d40be7aa2ac49818526
ariyosheva <115518111+ariyosheva@users.noreply.github.com>
2022-12-11 13:09:40 +0700

    Update Main_3003.java

--- Main_3003.java
+++ Main_3003.java
@@ -12,16 +12,15 @@ package com.mycompanyject.uts_no3_05a_3003;
 public class UTS_NO3_05A_3003 {
 
      public static void main(String[] args) {
-        Asprak_3003 A = new Asprak_3003("21103003", "Pinsen", "Sistem Informasi", 3, "PBO", 12);
+        Asprak_3003 A = new Asprak_3003("21103003", "Pinsen", "Sistem Informasi", 3, "PBO", 2);
         A.tampilDataMhs();
-        System.out.println("Total : " + A.totalPendapatan_3003());
+        System.out.println("Total : 600000" + A.totalPendapatan_3003());
         System.out.println("");
 
-        StudentStaff_3003 B = new StudentStaff_3003("211480", "Faruk", "Sistem Informasi", 4, 2, 8);
+        StudentStaff_3003 B = new StudentStaff_3003("211480", "Faruk", "Sistem Informasi", 4, "Informatika", 12);
         B.tampilDataMhs();
-        System.out.println("Total : " + B.totalPendapatan_3003());
+        System.out.println("Total : 600000" + B.totalPendapatan_3003());
         System.out.println("");
 
     }
 }
-
